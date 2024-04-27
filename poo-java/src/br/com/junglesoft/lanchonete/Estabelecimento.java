82c03a61e2e11cb33e3f7e1db4093ba45a5ffaa9
Alexandre Moreira <4lexandre.moreira@gmail.com>
2024-04-27 11:53:44 -0300

    feat: acoes coziheiro na  lanchonete

--- poo-java/src/br/com/junglesoft/lanchonete/Estabelecimento.java
+++ poo-java/src/br/com/junglesoft/lanchonete/Estabelecimento.java
@@ -1,5 +1,19 @@
 package br.com.junglesoft.lanchonete;
 
 public class Estabelecimento {
+    public static void main(String[] args) {
+        Cozinheiro cozinheiro = new Cozinheiro();
+        cozinheiro.lavarIngredientes();
+        cozinheiro.baterVitaminaLiquidificador();
+        cozinheiro.selecionarIngredientesVitamina();
+        cozinheiro.prepararLanche();
+        cozinheiro.prepararVitamina();
+        cozinheiro.prepararVitamina();
+
+        cozinheiro.adicionarSucoNoBalcao();
+        cozinheiro.adicionarLancheNoBalcao();
+        cozinheiro.adicionarComboNoBalcao();
+
+    }
     
 }
