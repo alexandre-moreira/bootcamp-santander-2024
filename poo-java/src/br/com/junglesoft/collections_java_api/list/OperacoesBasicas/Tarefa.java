44dd67a36ad5fbc6b25d14c9e0664361383061aa
Alexandre Moreira <4lexandre.moreira@gmail.com>
2024-05-02 17:19:33 -0300

    feat: Collections, list - lista de tarefas

---
+++ poo-java/src/br/com/junglesoft/collections_java_api/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,24 @@
+package br.com.junglesoft.collections_java_api.list.OperacoesBasicas;
+
+public class Tarefa {
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return  descricao;
+    }
+
+    
+}
