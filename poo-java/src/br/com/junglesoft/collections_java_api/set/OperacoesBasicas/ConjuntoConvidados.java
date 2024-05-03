7b6e2fe1529ccbbfb27c9044b5c5b1b3b402cb21
Alexandre Moreira <4lexandre.moreira@gmail.com>
2024-05-03 13:18:52 -0300

    feat: Operacoes basicas Set

---
+++ poo-java/src/br/com/junglesoft/collections_java_api/set/OperacoesBasicas/ConjuntoConvidados.java
@@ -0,0 +1,38 @@
+package br.com.junglesoft.collections_java_api.set.OperacoesBasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite){
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(int codigoConvite){
+        Convidado convidadoParaRemover = null;
+        for(Convidado c : convidadoSet){
+            if(c.getCodigoConvite() == codigoConvite){
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidado(){
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados(){
+        System.out.println(convidadoSet);
+    }
+
+
+    
+}
