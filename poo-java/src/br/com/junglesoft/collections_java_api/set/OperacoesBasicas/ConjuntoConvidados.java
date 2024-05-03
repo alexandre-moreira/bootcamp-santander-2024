142143cbd52d4a8b7d3abf0f06549047037a079c
Alexandre Moreira <4lexandre.moreira@gmail.com>
2024-05-03 14:45:06 -0300

    feat: Operacoes basicas Set - Testando os metodos

--- poo-java/src/br/com/junglesoft/collections_java_api/set/OperacoesBasicas/ConjuntoConvidados.java
+++ poo-java/src/br/com/junglesoft/collections_java_api/set/OperacoesBasicas/ConjuntoConvidados.java
@@ -33,6 +33,21 @@ public class ConjuntoConvidados {
         System.out.println(convidadoSet);
     }
 
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
 
-    
+        System.out.println("Existem "  + conjuntoConvidados.contarConvidado() + " dentro do Set de Convidados.");
+        
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 2", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 3", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 4", 1236);
+        
+        System.out.println("Existem "  + conjuntoConvidados.contarConvidado() + " dentro do Set de Convidados.");
+        
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1234);
+        System.out.println("Existem "  + conjuntoConvidados.contarConvidado() + " dentro do Set de Convidados.");
+
+        conjuntoConvidados.exibirConvidados();
+    }
 }
