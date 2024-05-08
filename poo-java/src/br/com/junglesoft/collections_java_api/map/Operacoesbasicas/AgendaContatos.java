54bd061b042f705f5c031da20b8fd34d7a835f4d
Alexandre Moreira <4lexandre.moreira@gmail.com>
2024-05-08 11:39:51 -0300

    feat: Operacoes basicas Map

---
+++ poo-java/src/br/com/junglesoft/collections_java_api/map/Operacoesbasicas/AgendaContatos.java
@@ -0,0 +1,35 @@
+package br.com.junglesoft.collections_java_api.map.Operacoesbasicas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class AgendaContatos {
+    private Map<String, Integer> agendaContatoMap;
+
+    public AgendaContatos(){
+        this.agendaContatoMap = new HashMap<>();
+    }
+
+    public void adicionarContato(String nome, Integer telefone){
+        agendaContatoMap.put(nome, telefone);
+    }
+
+    public void removerContato(String nome){
+        if(!agendaContatoMap.isEmpty()){
+            agendaContatoMap.remove(nome);
+        }
+    }
+
+    public void exibirContatos(){
+        System.out.println(agendaContatoMap);
+    }
+
+    public Integer pesquisaPorNome(String nome){
+        Integer numeroPorNome = null;
+        if(!agendaContatoMap.isEmpty()){
+            numeroPorNome = agendaContatoMap.get(nome);
+        }
+        return numeroPorNome;
+    }
+}
---
+++ poo-java/src/br/com/junglesoft/collections_java_api/map/Readme.md
@@ -0,0 +1,21 @@
+# Fixando os conhecimentos
+
+Exercicios:
+    1.Operacoes Basicas com Map.
+    2. Pesquisa em Map
+    3. Ordenacao nas Map
+
+# Operacoes Básicas com Map
+
+## 1. Agenda de Contatos
+    Crie uma classe chamada "AgendaContatos", que utilize um Map para armazenar os contatos. Cada contato possui um nome como chave e um numero de telefone como valor. Implemente os seguintes metodos:
+
+    * adicionarContato(String nome, Integer telefone): Adiciona um contato a agenda, associando o nome do contato ao numero de telefone correspondente.
+
+    * removerContato(String nome): Remove o contato da agenda, dado o nome do contato.
+
+    * exibirContatos(): Exibe todos os contatos da agenda, mostrando o nome e o numero de telefone de cada contato.
+
+    * pesquisarPorNome(String nome): Pesquisa um contato pelo nome e retorna o numero de telefone correspondente.
+
+    
