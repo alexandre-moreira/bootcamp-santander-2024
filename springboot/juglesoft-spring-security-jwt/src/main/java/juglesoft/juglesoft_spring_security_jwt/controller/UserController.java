6d1c5371555e24b5d38ca48c0dbda2e6e41f1884
Alexandre Moreira <4lexandre.moreira@gmail.com>
2024-05-31 19:18:10 -0300

    feat: userController

---
+++ springboot/juglesoft-spring-security-jwt/src/main/java/juglesoft/juglesoft_spring_security_jwt/controller/UserController.java
@@ -0,0 +1,20 @@
+package juglesoft.juglesoft_spring_security_jwt.controller;
+
+import juglesoft.juglesoft_spring_security_jwt.model.User;
+import juglesoft.juglesoft_spring_security_jwt.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService service;
+    @PostMapping
+    public void postUser(@RequestBody User user){
+        service.createUser(user);
+    }
+}
