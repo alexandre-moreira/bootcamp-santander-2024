47193f1a85bcb0b7bd53f26b02cb72f0d51fd59e
Alexandre Moreira <4lexandre.moreira@gmail.com>
2024-05-31 19:16:46 -0300

    feat: model user

---
+++ springboot/juglesoft-spring-security-jwt/src/main/java/juglesoft/juglesoft_spring_security_jwt/JuglesoftSpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package juglesoft.juglesoft_spring_security_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuglesoftSpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuglesoftSpringSecurityJwtApplication.class, args);
+	}
+
+}
