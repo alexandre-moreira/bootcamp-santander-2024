dd5f988f08f195fd851d693b7c354d54d9b39cc7
Alexandre Moreira <4lexandre.moreira@gmail.com>
2024-05-31 19:15:21 -0300

    feat: project base jwt

---
+++ springboot/juglesoft-spring-security-jwt/src/test/java/juglesoft/juglesoft_spring_security_jwt/JuglesoftSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package juglesoft.juglesoft_spring_security_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuglesoftSpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
